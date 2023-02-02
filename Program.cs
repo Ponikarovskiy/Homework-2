f60f4293fb852819e14e3f31c9eef0a8fa49e02a
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-02-02 23:37:00 +0300

    решена 13-я задача

--- Program.cs
+++ Program.cs
@@ -7,11 +7,46 @@
 // 782 -> 8
 // 918 -> 1
 
+// System.Console.WriteLine();
+// System.Console.WriteLine("*Задача № 10. Вывести вторую цифру из трехзначного числа*");
+// System.Console.WriteLine();
+
+// System.Console.WriteLine("Введите трехзначное число");
+
+// if (!int.TryParse(Console.ReadLine(), out int number))
+// {
+//     Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+// }
+
+// if (number < 0)
+// {
+//     Console.WriteLine("Ввод неверный. Пожалуйста, введите положительное число");
+//     return;
+// }
+
+// int length = number.ToString().Length;
+
+// if (length == 3)
+// {
+//     int result = number / 10 % 10;
+//     System.Console.WriteLine(result);
+// }
+// else
+// {
+//     System.Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+// }
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
 System.Console.WriteLine();
-System.Console.WriteLine("*Задача № 10. Вывести вторую цифру из трехзначного числа*");
+System.Console.WriteLine("*Задача № 13. Вывести 3-ю цифру из заданного числа*");
 System.Console.WriteLine();
 
-System.Console.WriteLine("Введите трехзначное число");
+System.Console.WriteLine("Введите число");
 
 if (!int.TryParse(Console.ReadLine(), out int number))
 {
@@ -21,26 +56,27 @@ if (!int.TryParse(Console.ReadLine(), out int number))
 if (number < 0)
 {
     Console.WriteLine("Ввод неверный. Пожалуйста, введите положительное число");
-    return;
 }
 
 int length = number.ToString().Length;
 
-if (length == 3)
+if (length >= 3)
 {
-    int result = number / 10 % 10;
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
     System.Console.WriteLine(result);
 }
 else
 {
-    System.Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+    System.Console.WriteLine("Третьей цифры нет");
 }
 
 
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+
+
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
