4a968b64b0f35db73446345da303fd71d2f639f5
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-02-02 23:51:07 +0300

    решена 15-я задача

--- Program.cs
+++ Program.cs
@@ -42,37 +42,37 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-System.Console.WriteLine();
-System.Console.WriteLine("*Задача № 13. Вывести 3-ю цифру из заданного числа*");
-System.Console.WriteLine();
+// System.Console.WriteLine();
+// System.Console.WriteLine("*Задача № 13. Вывести 3-ю цифру из заданного числа*");
+// System.Console.WriteLine();
 
-System.Console.WriteLine("Введите число");
+// System.Console.WriteLine("Введите число");
 
-if (!int.TryParse(Console.ReadLine(), out int number))
-{
-    Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
-}
+// if (!int.TryParse(Console.ReadLine(), out int number))
+// {
+//     Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+// }
 
-if (number < 0)
-{
-    Console.WriteLine("Ввод неверный. Пожалуйста, введите положительное число");
-}
+// if (number < 0)
+// {
+//     Console.WriteLine("Ввод неверный. Пожалуйста, введите положительное число");
+// }
 
-int length = number.ToString().Length;
+// int length = number.ToString().Length;
 
-if (length >= 3)
-{
-    while (number > 999)
-    {
-        number = number / 10;
-    }
-    int result = number % 10;
-    System.Console.WriteLine(result);
-}
-else
-{
-    System.Console.WriteLine("Третьей цифры нет");
-}
+// if (length >= 3)
+// {
+//     while (number > 999)
+//     {
+//         number = number / 10;
+//     }
+//     int result = number % 10;
+//     System.Console.WriteLine(result);
+// }
+// else
+// {
+//     System.Console.WriteLine("Третьей цифры нет");
+// }
 
 
 
@@ -82,3 +82,30 @@ else
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+System.Console.WriteLine();
+System.Console.WriteLine("*Задача № 15. Проверка на выходной день*");
+System.Console.WriteLine();
+
+System.Console.WriteLine("Введите день недели:");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day >= 1 && day <= 7)
+{
+    if (day >= 6)
+    {
+        System.Console.WriteLine("да");
+    }
+    else
+    {
+        System.Console.WriteLine("нет");
+    }
+}
+else
+{
+    System.Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+}
+
+
+
