8b5d68e481b00a1ea6430bb5c3fe64c9fe29c3cb
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-02-02 23:54:15 +0300

    Финиш

--- Program.cs
+++ Program.cs
@@ -83,29 +83,29 @@
 // 7 -> да
 // 1 -> нет
 
-System.Console.WriteLine();
-System.Console.WriteLine("*Задача № 15. Проверка на выходной день*");
-System.Console.WriteLine();
+// System.Console.WriteLine();
+// System.Console.WriteLine("*Задача № 15. Проверка на выходной день*");
+// System.Console.WriteLine();
 
-System.Console.WriteLine("Введите день недели:");
+// System.Console.WriteLine("Введите день недели:");
 
-int day = Convert.ToInt32(Console.ReadLine());
+// int day = Convert.ToInt32(Console.ReadLine());
 
-if (day >= 1 && day <= 7)
-{
-    if (day >= 6)
-    {
-        System.Console.WriteLine("да");
-    }
-    else
-    {
-        System.Console.WriteLine("нет");
-    }
-}
-else
-{
-    System.Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
-}
+// if (day >= 1 && day <= 7)
+// {
+//     if (day >= 6)
+//     {
+//         System.Console.WriteLine("да");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("нет");
+//     }
+// }
+// else
+// {
+//     System.Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+// }
 
 
 
