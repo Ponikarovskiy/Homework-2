72a9d2f0f1f88f1d02d0be8785ece86d61e55f89
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-02-02 23:18:13 +0300

    решена 1-я задача

--- Program.cs
+++ Program.cs
@@ -7,6 +7,36 @@
 // 782 -> 8
 // 918 -> 1
 
+System.Console.WriteLine();
+System.Console.WriteLine("*Задача № 10. Вывести вторую цифру из трехзначного числа*");
+System.Console.WriteLine();
+
+System.Console.WriteLine("Введите трехзначное число");
+
+if (!int.TryParse(Console.ReadLine(), out int number))
+{
+    Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+}
+
+if (number < 0)
+{
+    Console.WriteLine("Ввод неверный. Пожалуйста, введите положительное число");
+    return;
+}
+
+int length = number.ToString().Length;
+
+if (length == 3)
+{
+    int result = number / 10 % 10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+}
+
+
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
